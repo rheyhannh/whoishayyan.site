bd531911f263402752e5b77a7c294f2c0404311d
rheyhannh <rheyhannh@student.ub.ac.id>
2023-10-20 17:05:25 +0700

    fix: Import font with url instead next/font/google

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,4 @@
-import { Poppins } from 'next/font/google';
 import './globals.css'
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-poppins',
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
-});
 
 export const metadata = {
   title: 'Who is Hayyan',
@@ -17,7 +9,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html className={`${poppins.variable}`}>
+    <html>
       <body>
         {children}
       </body>
