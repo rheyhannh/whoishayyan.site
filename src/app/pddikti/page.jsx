35b2296551bf79a54d929af61e57447d6839932e
rheyhannh <rheyhannh@student.ub.ac.id>
2023-10-20 22:48:26 +0700

    fix: /pddikti css files

--- src/app/pddikti/page.jsx
+++ src/app/pddikti/page.jsx
@@ -1,14 +1,12 @@
-import styles from '../page.module.css'
+import styles from './_pddikti.module.css'
 import Link from 'next/link'
 
-export default function Pddikti () {
+export default function Pddikti() {
     return (
-        <main className={styles.default}>
-            <div>
-                <h1>PDDIKTI Search Page.</h1>
-                <p>Cari data mahasiswa, universitas dan lainnya!</p>
-                <Link href="/">Back to Home</Link>
-            </div>
-        </main>
+        <div className={styles.default}>
+            <h1>PDDIKTI Search Page.</h1>
+            <p>Cari data mahasiswa, universitas dan lainnya!</p>
+            <Link href="/">Back to Home</Link>
+        </div>
     )
 }
