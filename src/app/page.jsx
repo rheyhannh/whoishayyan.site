ccc12788043ba5c37e052015671064188f344bf3
rheyhannh <rheyhannh@student.ub.ac.id>
2023-11-06 14:52:09 +0700

    refactor

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,16 +2,15 @@ import { ThemeProvider } from '@/components/Theme'
 import Header from '@/components/Header'
 import Sections from '@/components/Sections'
 import Footer from '@/components/Footer'
-import styles from './_root.module.css'
+import Scrollup from '@/components/Scrollup'
 
 export default function Home() {
   return (
     <ThemeProvider>
       <Header />
-      <main className={styles.main}>
-        <Sections />
-      </main>
+      <Sections />
       <Footer />
+      <Scrollup />
     </ThemeProvider>
   )
 }
