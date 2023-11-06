db1f97af9d3677dbba7271e7f6ce38e843d74293
rheyhannh <rheyhannh@student.ub.ac.id>
2023-11-06 14:50:53 +0700

    feat: add Webvitals and Scrollup component

---
+++ src/components/Scrollup.jsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { useEffect, useState } from 'react';
+import Link from 'next/link'
+import { UilArrowUp } from '@iconscout/react-unicons'
+import styles from '../app/_root.module.css'
+
+export default function Scrollup() {
+    const [scrollUp, setScrollUp] = useState(false);
+
+    const handleScrollUp = () => {
+        if (window.scrollY >= 560) { setScrollUp(true) }
+        else { setScrollUp(false) }
+    }
+
+    useEffect(() => {
+        if (window.scrollY >= 560) { setScrollUp(true) }
+
+        window.addEventListener('scroll', handleScrollUp);
+
+        return () => {
+            window.removeEventListener('scroll', handleScrollUp);
+        }
+    })
+    return (
+        <Link
+            href={'#home'}
+            className={`${styles.scrollup} ${scrollUp ? styles.show_scroll : ''}`}
+        >
+            <UilArrowUp className={styles.scrollup_icon} />
+        </Link>
+    )
+}
