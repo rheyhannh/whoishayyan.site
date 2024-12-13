22f050437b191f442fe2d4dae28ab13ada185834
rheyhannh <rheyhannh@student.ub.ac.id>
2024-12-14 01:32:15 +0700

    docs: Add method docs

--- src/utils/getUniconIcons.js
+++ src/utils/getUniconIcons.js
@@ -1,3 +1,11 @@
+/**
+ * Dynamically imports a specified Unicons icon component and renders it with a provided class name.
+ *
+ * @async
+ * @param {string} iconName Icon name in  `react-unicons`
+ * @param {string} className The CSS class name to apply
+ * @returns {Promise<JSX.Element|null>} A promise that resolves rendered icon component if the icon is found, or `null` if the icon does not exist
+ */
 const getUniconsIcons = async (iconName, className) => {
     const iconsModule = await import('@iconscout/react-unicons');
     if (iconName in iconsModule) {
