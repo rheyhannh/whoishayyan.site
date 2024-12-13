9cebf1cb4315f524af822842300c17f9340cb064
rheyhannh <rheyhannh@student.ub.ac.id>
2024-12-13 23:41:11 +0700

    feat: Add modal setting component

---
+++ src/components/modal/Setting.jsx
@@ -0,0 +1,58 @@
+import { useContext } from "react"
+import { ModalContext } from "../provider/Modal"
+import { RootPageContext } from "../provider/RootPage";
+
+import styles from './modal.module.css'
+
+/**
+ * Component Description
+ * @returns {JSX.Element} Rendered component
+ */
+export default function ModalSetting() {
+    const { active, handleModalClose } = useContext(ModalContext);
+    const { forceLoadingState, forceErrorState, setForceLoadingState, setForceErrorState } = useContext(RootPageContext);
+
+    return (
+        <div className={`${styles.overlay} ${active ? styles.active : ''}`}>
+            <div className={styles.setting}>
+                <h2 className={styles.heading}>
+                    <span>Setting</span>
+                    <span onClick={() => { handleModalClose() }}>X</span>
+                </h2>
+                <div className={styles.layout}>
+                    <span className={styles.small}>Page State</span>
+
+                    <div
+                        className={`${styles.panel} ${!forceErrorState && !forceLoadingState ? styles.active : ''}`}
+                        onClick={() => { setForceLoadingState(false); setForceErrorState(false); }}
+                    >
+                        <span className={styles.title}>Default</span>
+                        <span className={styles.small}>
+                            Normal view
+                        </span>
+                    </div>
+
+                    <div
+                        className={`${styles.panel} ${forceLoadingState ? styles.active : ''}`}
+                        onClick={() => { setForceLoadingState(true); setForceErrorState(false); }}
+                    >
+                        <span className={styles.title}>Loading</span>
+                        <span className={styles.small}>
+                            Simulate the view during loading
+                        </span>
+                    </div>
+
+                    <div
+                        className={`${styles.panel} ${forceErrorState ? styles.active : ''}`}
+                        onClick={() => { setForceErrorState(true); setForceLoadingState(false); }}
+                    >
+                        <span className={styles.title}>Error</span>
+                        <span className={styles.small}>
+                            Simulate the view during error
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
