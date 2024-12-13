7614fc1b5386955473e6ec906c4df9c447c40dcd
rheyhannh <rheyhannh@student.ub.ac.id>
2024-12-13 23:46:00 +0700

    fix: Remove unused wrapper element

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -73,94 +73,92 @@ export default function Header() {
     ]
 
     return (
-        <>
-            <div>
-                <header className={styles.header} id="header">
+        <header className={styles.header} id="header">
+            <nav className={`${styles.nav} ${styles.container}`} id="header">
+                <Link
+                    href="#home"
+                    className={`${styles.nav__logo} ${styles.active}`}
+                    id="logoText"
+                >
+                    <span>Hayyan</span>
+                </Link>
 
-                    <nav className={`${styles.nav} ${styles.container}`} id="header">
-                        <Link
-                            href="#home"
-                            className={`${styles.nav__logo} ${styles.active}`}
-                            id="logoText"
-                        >
-                            <span>Hayyan</span>
-                        </Link>
+                <div className={`${styles.nav__menu} ${menu ? styles.show_menu : ''}`} id="nav-menu">
+                    <ul className={`${styles.nav__list} ${styles.grid}`}>
+                        {navList.map((item, index) => (
+                            <li key={crypto.randomUUID()} className={styles.nav__item}>
+                                <Link
+                                    href={item.href}
+                                    className={`${styles.nav__link} ${index === 0 ? styles.active_link : ''}`}
+                                >
+                                    <i className={index === 0 ? '' : styles.nav__icon}>{item.uil}</i>
+                                    <span>{item.text}</span>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <i className={styles.nav__close} onClick={toggleMenu} id="nav-close"><UilTimes /></i>
+                </div>
 
-                        <div className={`${styles.nav__menu} ${menu ? styles.show_menu : ''}`} id="nav-menu">
-                            <ul className={`${styles.nav__list} ${styles.grid}`}>
-                                {navList.map((item, index) => (
-                                    <li key={crypto.randomUUID()} className={styles.nav__item}>
-                                        <Link
-                                            href={item.href}
-                                            className={`${styles.nav__link} ${index === 0 ? styles.active_link : ''}`}
-                                        >
-                                            <i className={index === 0 ? '' : styles.nav__icon}>{item.uil}</i>
-                                            <span>{item.text}</span>
-                                        </Link>
-                                    </li>
-                                ))}
-                            </ul>
-                            <i className={styles.nav__close} onClick={toggleMenu} id="nav-close"><UilTimes /></i>
-                        </div>
-
-                        <div className={`${styles.nav__menu_hue} ${hueMenu ? styles.show_menu : ''}`}>
-                            <p style={{
-                                margin: '0 0 1.25rem 0',
-                                textAlign: 'center',
-                                fontWeight: 'var(--font-semi-bold)'
-                            }}>Select Color Theme</p>
-                            <div className={`${styles.nav__list_hue} ${styles.grid}`}>
-                                {palleteList.map((item, index) => (
-                                    <div
-                                        className={`${styles.nav__hue} ${hue === item.hue ? styles.active : ''}`}
-                                        hue={item.hue}
-                                        style={{ background: `hsl(${item.hue}, 69%, 61%)` }}
-                                        key={crypto.randomUUID()}
-                                        onClick={() => { handleHue(item.hue) }}
-                                    >
-                                        {hue === item.hue ? <UilCheckCircle /> : ''}
-                                    </div>
-                                ))}
+                <div className={`${styles.nav__menu_hue} ${hueMenu ? styles.show_menu : ''}`}>
+                    <p style={{
+                        margin: '0 0 1.25rem 0',
+                        textAlign: 'center',
+                        fontWeight: 'var(--font-semi-bold)'
+                    }}>Select Color Theme</p>
+                    <div className={`${styles.nav__list_hue} ${styles.grid}`}>
+                        {palleteList.map((item, index) => (
+                            <div
+                                className={`${styles.nav__hue} ${hue === item.hue ? styles.active : ''}`}
+                                hue={item.hue}
+                                style={{ background: `hsl(${item.hue}, 69%, 61%)` }}
+                                key={crypto.randomUUID()}
+                                onClick={() => { handleHue(item.hue) }}
+                            >
+                                {hue === item.hue ? <UilCheckCircle /> : ''}
                             </div>
+                        ))}
+                    </div>
 
-                            <i className={styles.nav__close} onClick={toggleHueMenu} id="nav-close"><UilTimes /></i>
-                        </div>
+                    <i className={styles.nav__close} onClick={toggleHueMenu} id="nav-close"><UilTimes /></i>
+                </div>
 
-                        <div className={styles.nav__slider}>
-                            <input
-                                className={styles.color_range}
-                                style={{
-                                    color: `hsl(${hue}, 69%, 61%)`
-                                }}
-                                value={`${hue}`}
-                                onChange={(e) => {
-                                    handleHue(e.target.valueAsNumber);
-                                }}
-                                type="range"
-                                min="0"
-                                max="359"
-                                name='colorrange'
-                                id='colorrange'
-                            />
-                        </div>
+                <div className={styles.nav__slider}>
+                    <input
+                        className={styles.color_range}
+                        style={{
+                            color: `hsl(${hue}, 69%, 61%)`
+                        }}
+                        value={`${hue}`}
+                        onChange={(e) => {
+                            handleHue(e.target.valueAsNumber);
+                        }}
+                        type="range"
+                        min="0"
+                        max="359"
+                        name='colorrange'
+                        id='colorrange'
+                    />
+                </div>
 
-                        <div className={styles.nav__btns}>
-                            <i className={styles.change_theme} onClick={toggleTheme}>
-                                {theme === "dark" ? <UilSun /> : <UilMoon />}
-                            </i>
+                <div className={styles.nav__btns}>
+                    <i className={styles.config} onClick={toggleSettingModal}>
+                        <UilSetting />
+                    </i>
 
-                            <i className={`${styles.change_theme} ${styles.hue}`} onClick={toggleHueMenu}>
-                                <UilPalette />
-                            </i>
+                    <i className={styles.change_theme} onClick={toggleTheme}>
+                        {theme === "dark" ? <UilSun /> : <UilMoon />}
+                    </i>
 
-                            <div className={styles.nav__toggle} onClick={toggleMenu} id="nav-toggle">
-                                <i><UilApps /></i>
-                            </div>
-                        </div>
-                    </nav>
-                </header>
-            </div>
+                    <i className={`${styles.change_theme} ${styles.hue}`} onClick={toggleHueMenu}>
+                        <UilPalette />
+                    </i>
 
-        </>
+                    <div className={styles.nav__toggle} onClick={toggleMenu} id="nav-toggle">
+                        <i><UilApps /></i>
+                    </div>
+                </div>
+            </nav>
+        </header>
     )
 }
