345356d9a77ff933b96d714fa658001de88a4a42
rheyhannh <rheyhannh@student.ub.ac.id>
2024-12-14 02:10:18 +0700

    fix: Remove unused wrapper element

--- src/components/project/Data.jsx
+++ src/components/project/Data.jsx
@@ -52,68 +52,64 @@ export default function ProjectData({ data }) {
     }, [data, otherIcons])
 
     return (
-        <>
-            <Swiper
-                modules={[Navigation, Pagination]}
-                loop={data.length > 2 ? true : false}
-                cssMode={true}
-                navigation={{
-                    nextEl: `.${styles.swiper_button_next}`,
-                    prevEl: `.${styles.swiper_button_prev}`,
-                }}
-                pagination={{
-                    el: ".swiper-pagination",
-                    clickable: true,
-                }}
-                className={`${styles.portfolio__container} ${styles.container}`}
-            >
-                {data.map((item, index) => (
-                    <>
-                        <SwiperSlide
-                            className={`${styles.portfolio__content} ${styles.grid}`}
-                            key={crypto.randomUUID()}
-                        >
-                            <Image
-                                src={item.image.src}
-                                width={300}
-                                height={200}
-                                quality={100}
-                                alt={item.image.alt}
-                                className={styles.portfolio__img}
-                            />
-                            <div className={styles.portfolio__data}>
-                                <h3 className={styles.portfolio__title}>{item.title}</h3>
-                                <p className={`${styles.portfolio__description} ${styles.text__justify}`} >
-                                    {item.description}
-                                </p>
-                                <Button
-                                    href={item.button.href}
-                                    target={item.button.newTab ? '_blank' : '_self'}
-                                    className={`${styles.button} ${styles.button__flex} ${styles.button__small} ${styles.portfolio__button}`}
-                                    text={item.button.text}
-                                    icon={projectIcons[index]}
-                                    label='Details'
-                                />
-                            </div>
-                        </SwiperSlide>
-                    </>
-                ))}
+        <Swiper
+            modules={[Navigation, Pagination]}
+            loop={data.length > 2 ? true : false}
+            cssMode={true}
+            navigation={{
+                nextEl: `.${styles.swiper_button_next}`,
+                prevEl: `.${styles.swiper_button_prev}`,
+            }}
+            pagination={{
+                el: ".swiper-pagination",
+                clickable: true,
+            }}
+            className={`${styles.portfolio__container} ${styles.container}`}
+        >
+            {data.map((item, index) => (
+                <SwiperSlide
+                    className={`${styles.portfolio__content} ${styles.grid}`}
+                    key={crypto.randomUUID()}
+                >
+                    <Image
+                        src={item.image.src}
+                        width={300}
+                        height={200}
+                        quality={100}
+                        alt={item.image.alt}
+                        className={styles.portfolio__img}
+                    />
+                    <div className={styles.portfolio__data}>
+                        <h3 className={styles.portfolio__title}>{item.title}</h3>
+                        <p className={`${styles.portfolio__description} ${styles.text__justify}`} >
+                            {item.description}
+                        </p>
+                        <Button
+                            href={item.button.href}
+                            target={item.button.newTab ? '_blank' : '_self'}
+                            className={`${styles.button} ${styles.button__flex} ${styles.button__small} ${styles.portfolio__button}`}
+                            text={item.button.text}
+                            icon={projectIcons[index]}
+                            label='Details'
+                        />
+                    </div>
+                </SwiperSlide>
+            ))}
 
-                {data.length > 1 &&
-                    <>
-                        <div className={styles.swiper_button_prev}>
-                            {icons[0]}
-                        </div>
-                        <div className={styles.swiper_button_next}>
-                            {icons[1]}
-                        </div>
+            {data.length > 1 &&
+                <>
+                    <div className={styles.swiper_button_prev}>
+                        {icons[0]}
+                    </div>
+                    <div className={styles.swiper_button_next}>
+                        {icons[1]}
+                    </div>
 
-                        <div className={`${styles.portfolio} ${styles.pagination}`}>
-                            <div className="swiper-pagination"></div>
-                        </div>
-                    </>
-                }
-            </Swiper>
-        </>
+                    <div className={`${styles.portfolio} ${styles.pagination}`}>
+                        <div className="swiper-pagination"></div>
+                    </div>
+                </>
+            }
+        </Swiper>
     )
 }
