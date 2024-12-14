3fb1ec583b43762205058ce56e60210b27982248
rheyhannh <rheyhannh@student.ub.ac.id>
2024-12-15 00:14:21 +0700

    docs: Update utils docs

--- src/utils/handleClientError.js
+++ src/utils/handleClientError.js
@@ -3,14 +3,26 @@ import { isZodErrorLike, fromZodError } from 'zod-validation-error';
 /**
  * Method that handles exception in client-side with this behaviour, 
  * 
- * - When application run in `production`, message will be logged in browser console
- * - When application run in `!production` environment, both message and error will be logged in browser console
+ * - When application run in `production`, error message will be logged in browser console
+ * - When application run in `!production` environment, both error message and error will be logged in browser console
  * - When environment variable `NEXT_PUBLIC_THIRDPARTY_LOG` values are truthy, error will sended to third party service
- *  
- * Knowing `message` will exposed to client browser, make sure to sanitize any sensitive information before passing to this handler.
  * 
- * @param {string} message Sanitized error message
- * @param {any} error Error details
+ * This method will generate error message that easier to read for end users using {@link https://github.com/causaly/zod-validation-error?tab=readme-ov-file#input-from-zod zod-validation-error}
+ * when error are instance of `zodError`
+ * 
+ * @param {string} prefix Prefix to logged in browser console along with error message that can be an event, method name or error type. Also used as zod-validation-error {@link https://github.com/causaly/zod-validation-error?tab=readme-ov-file#fromzoderror prefix options} when error are `zodError`
+ * @param {any} error Any error object or instance
+ * @example 
+ * ```js
+ * const getResource = () => {
+ *      try { } // doSomething 
+ *      catch(error) {
+ *          // assume error.message = 'internal server error'
+ *          handleClientError('Failed to get resource', error)
+ *          // logged in browser 'Failed to get resource: internal server error'
+ *      }
+ * }
+ * ```
  */
 const handleClientError = (prefix, error) => {
     const isProduction = process.env.NODE_ENV === 'production';
