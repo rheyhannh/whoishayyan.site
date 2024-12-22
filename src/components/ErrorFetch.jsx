ffc2e748d49f3324ccd3d47ad4533874cc89862d
rheyhannh <rheyhannh@student.ub.ac.id>
2024-12-22 23:19:38 +0700

    feat: Improve keyboard focusability

--- src/components/ErrorFetch.jsx
+++ src/components/ErrorFetch.jsx
@@ -3,10 +3,15 @@ import styles from '@/app/_root.module.css'
 export default function ErrorFetch({ clickEvent, type }) {
     return (
         <div className={styles.error__fetch}>
-            <div className={styles.error__fetch_msg} onClick={clickEvent}>
-                <h3>Gagal mengambil {type ? type + ' data' : 'data'}</h3>
-                <p>Muat Ulang &#x21bb;</p>
+            <div
+                tabIndex={'0'}
+                className={styles.error__fetch_msg}
+                onClick={clickEvent}
+                onKeyDown={(e) => { if (e.key === 'Enter') clickEvent(); }}
+            >
+                <h3>Could not load {type ? type + ' data' : 'data'}</h3>
+                <p>Click to Retry &#x21bb;</p>
             </div>
-        </div>        
+        </div>
     )
 }
