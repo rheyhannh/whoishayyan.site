f85d6103fd2f1206740020b8ed153391fa8db0ab
rheyhannh <rheyhannh@student.ub.ac.id>
2024-12-22 23:53:48 +0700

    fix: Adjust style not in body

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,16 +4,27 @@ import Header from '@/components/Header'
 import Sections from '@/components/Sections'
 import Footer from '@/components/Footer'
 import Scrollup from '@/components/Scrollup'
+import styles from '@/app/_root.module.css'
 
 export default function Home() {
   return (
     <RootPageProvider>
       <ModalProvider>
-        <Header />
-        <Sections />
-        <Footer />
-        <Scrollup />
+        <Base>
+          <Header />
+          <Sections />
+          <Footer />
+          <Scrollup />
+        </Base>
       </ModalProvider>
     </RootPageProvider>
+  )
+}
+
+function Base({ children }) {
+  return (
+    <div className={styles.base}>
+      {children}
+    </div>
   )
 }
