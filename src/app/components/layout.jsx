4c1930a052f9c41e45d41383088e08c5262bf5c9
rheyhannh <rheyhannh@student.ub.ac.id>
2024-12-22 23:13:41 +0700

    feat: Add components page

---
+++ src/app/components/layout.jsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: 'Under Construction',
+  description: 'Components Showcase',
+  keywords: 'reyhan, naufal, hayyan, website, developer, frontend, backend, crypto, cryptocurrencies, blockchain, iot, computer, engineering',
+  author: 'Reyhan Naufal Hayyan',
+}
+
+export default function ComponentsLayout({ children }) {
+  return children
+}
