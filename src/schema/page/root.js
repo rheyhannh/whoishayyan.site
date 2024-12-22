dbdd1c3b80f28715c74984210881690e518fe053
rheyhannh <rheyhannh@student.ub.ac.id>
2024-12-22 23:20:36 +0700

    fix: Update schemas

--- src/schema/page/root.js
+++ src/schema/page/root.js
@@ -5,9 +5,15 @@ import { z } from 'zod';
  * @typedef {z.infer<typeof aboutSectionDataSchema>} aboutSectionDataType
  */
 export const aboutSectionDataSchema = z.object({
-    /** Section description */
+    /** 
+     * Section description 
+     * @required
+     */
     description: z.string(),
-    /** Array as a interested list */
+    /** 
+     * Array as a interested list 
+     * @required To keep layout, atleast 1 entries
+     */
     interestedList: z.array(z.string()),
 });
 
@@ -16,25 +22,44 @@ export const aboutSectionDataSchema = z.object({
  * @typedef {z.infer<typeof contactSectionDataSchema>} contactSectionDataType
  */
 export const contactSectionDataSchema = z.object({
-    /** Array that describe an content. This will render icon with title and description */
+    /** 
+     * Array that describe an content. This will render icon with title and description 
+     * @required To keep layout, atleast 1 content
+     */
     content: z.array(
         z.object({
             /** 
-             * Unicons icon name that exist in `react/unicons`
+             * Content icon that an Unicons icon name that exist in `react/unicons`
              * - Ex: `UilPhone`
+             * 
+             * @required
             */
             uil: z.string(),
-            /** Content title */
+            /** 
+             * Content title 
+             * @required
+             */
             title: z.string(),
-            /** Content description */
+            /** 
+             * Content description 
+             * @required
+             */
             text: z.string(),
-            /** Used classname */
-            className: z.string(),
+            /** 
+             * Used icon classname.
+             * This props are `nullish` so when null or undefined means not using icon classname.
+             * 
+             * @optional
+             */
+            className: z.string().nullish(),
         }),
     ),
     /** Object that describe button as call to action and rendered as anchor element */
     button: z.object({
-        /** Button text */
+        /** 
+         * Button text 
+         * @required
+         */
         text: z.string(),
     }),
 });
@@ -44,11 +69,20 @@ export const contactSectionDataSchema = z.object({
  * @typedef {z.infer<typeof homeSectionDataSchema>} homeSectionDataType
  */
 export const homeSectionDataSchema = z.object({
-    /** Section title */
+    /** 
+     * Section title 
+     * @required
+     */
     title: z.string(),
-    /** Section subtitle */
+    /** 
+     * Section subtitle 
+     * @required
+     */
     subtitle: z.string(),
-    /** Section description */
+    /** 
+     * Section description 
+     * @required
+     */
     description: z.string(),
 });
 
@@ -59,28 +93,67 @@ export const homeSectionDataSchema = z.object({
  */
 export const projectSectionDataSchema = z.array(
     z.object({
-        /** Object containing source and alt for project image */
+        /** 
+         * Object containing source and alt for project image 
+         * @required
+         */
         image: z.object({
             /** 
              * Project source image
              * - Ex : `/portfolio1.jpg`
+             * 
+             * @required
             */
             src: z.string(),
-            /** Project alt image */
+            /** 
+             * Project alt image 
+             * @required
+             */
             alt: z.string(),
         }),
-        /** Project title */
+        /** 
+         * Project title 
+         * @required
+         */
         title: z.string(),
-        /** Project description */
+        /** 
+         * Project description 
+         * @required
+         * */
         description: z.string(),
-        /** Object that describe button as call to action in current project item and rendered as anchor element */
+        /** 
+         * Object that describe button as call to action in current project item and rendered as anchor element 
+         * @required
+         */
         button: z.object({
-            /** Target that can be URL or element id that exist in current page */
+            /** 
+             * Target that can be URL or element id that exist in current page 
+             * @required
+             */
             href: z.string(),
-            /** When truthy will set target attribute `_blank` */
-            newTab: z.boolean(),
-            /** Button text */
+            /** 
+             * When truthy will set target attribute `_blank` to open link in new tab. 
+             * @default false
+             */
+            newTab: z.boolean().default(false),
+            /** 
+             * Button text 
+             * @required
+             */
             text: z.string(),
+            /** 
+             * Button icon that an Unicons icon name that exist in `react/unicons`. 
+             * - Ex: `UilPhone`
+             * 
+             * This props are `nullish` so when null or undefined means not using icon.
+             * @optional
+            */
+            iconName: z.string().nullish(),
+            /** 
+             * Button icon classname. This props are `nullish` so when null or undefined means not using icon classname.
+             * @optional
+            */
+            iconClass: z.string().nullish(),
         }),
     }),
 );
@@ -92,25 +165,45 @@ export const projectSectionDataSchema = z.array(
  */
 export const qualificationSectionDataSchema = z.array(
     z.object({
-        /** Tab title */
+        /** 
+         * Tab title 
+         * @required
+         */
         title: z.string(),
         /** 
          * Tab icon that an Unicons icon name that exist in `react/unicons`
          * - Ex: `UilPhone`
+         * 
+         * This props are `nullish` so when null or undefined means not using icon.
+         * @optional
         */
-        icon: z.string(),
-        /** Tab contents */
+        icon: z.string().nullish(),
+        /** 
+         * Tab contents 
+         * @required To keep layout, atleast 1 entries
+         */
         data: z.array(
             z.object({
-                /** Content title */
+                /** 
+                 * Content title 
+                 * @required
+                 */
                 title: z.string(),
-                /** Content target URL */
+                /** 
+                 * Content target URL 
+                 * @required
+                 */
                 href: z.string(),
-                /** Content description */
+                /** 
+                 * Content description 
+                 * @required
+                 */
                 subtitle: z.string(),
                 /** 
                  * Content date 
                  * - Ex : `2015 - 2018`
+                 * 
+                 * @required
                 */
                 date: z.string(),
             })
@@ -125,27 +218,47 @@ export const qualificationSectionDataSchema = z.array(
  */
 export const skillSectionDataSchema = z.array(
     z.object({
-        /** Item id that will react `key` used, so make sure each entries value are unique */
-        no: z.number(),
+        /** 
+         * Item id and should unique. Simple ways are leave this props as undefined, will generate random for it.
+         * @default Math.floor(Math.random() * 10000) + 1
+         */
+        no: z.number().default(Math.floor(Math.random() * 10000) + 1),
         /** 
          * Item logo image source 
          * - Ex : `/logo/axios.png`
+         * 
+         * @required
         */
         logo: z.string(),
         /** 
-         * Item logo image source while in dark mode, use `null` when not exist
+         * Item logo image source in dark mode.
+         * This props are `nullable` when not exist make sure use null.
          * - Ex : `/logo/axios.png`
+         * 
+         * @required
         */
         logoDark: z.string().nullable(),
-        /** Item tags */
+        /** 
+         * Item tags 
+         * @required Atleast 1 entries
+         */
         tags: z.array(z.string()),
         /** Item url
          * - Ex : `'https://react.dev/'`
+         * 
+         * This props are `nullish` so when null or undefined means will rendered as plain text not anchor.
+         * @optional
          */
         url: z.string(),
-        /** Item title */
+        /** 
+         * Item title 
+         * @required
+         */
         title: z.string(),
-        /** Item description */
+        /** 
+         * Item description 
+         * @required
+         */
         description: z.string(),
     }),
 );
@@ -155,17 +268,35 @@ export const skillSectionDataSchema = z.array(
  * @typedef {z.infer<typeof allSectionDataSchema>} allSectionDataType
  */
 export const allSectionDataSchema = z.object({
-    /** Home section data */
+    /** 
+     * Home section data 
+     * @required
+     */
     home: homeSectionDataSchema,
-    /** About section data */
+    /** 
+     * About section data 
+     * @required
+     */
     about: aboutSectionDataSchema,
-    /** Qualification section data */
+    /** 
+     * Qualification section data 
+     * @required
+     */
     qualification: qualificationSectionDataSchema,
-    /** Skills section data */
+    /** 
+     * Skills section data 
+     * @required
+     */
     skills: skillSectionDataSchema,
-    /** Project section data */
+    /** 
+     * Project section data
+     * @required
+     */
     project: projectSectionDataSchema,
-    /** Contact section data */
+    /** 
+     * Contact section data 
+     * @required
+     */
     contact: contactSectionDataSchema,
 })
 
